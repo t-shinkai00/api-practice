1481791a8f4d630296ae96e7cd07bf4d93538661
t-shinkai00 <t.shinkai00@gmail.com>
2021-01-17 02:38:48 +0900

    add input section

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import Fetch from "./Fetch";
 
 function App() {
+  const [input, setInput] = useState([]);
+  const [inputComplete, setInputComplete] = useState(false);
+  // useEffect(() => {
+  //   console.log(input);
+  // }, [input]);
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Enter Your GitHub name.</h1>
+        <input
+          onChange={(e) => {
+            // console.log(e.target.value);
+            setInput(e.target.value);
+            setInputComplete(false);
+          }}
+        />
+        <p>You entered: {input}</p>
+        <button onClick={() => setInputComplete(true)}>OK</button>
+        <Fetch input={input} complete={inputComplete}></Fetch>
       </header>
     </div>
   );
