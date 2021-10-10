4e947e569ad96515a729f8b402cd6142547c3fe2
t-shinkai00 <t.shinkai00@gmail.com>
2021-10-10 21:26:29 +0900

    set params

--- rakuten-api/get.py
+++ rakuten-api/get.py
@@ -1,5 +1,14 @@
 import requests
 import pandas as pd
-from secret import APPLICATIONID, ENDPOINT
 
+from secret import APPLICATIONID, ENDPOINT
 # print(APPLICATIONID, ENDPOINT)
+
+params={
+  "format":"json",
+  "largeClassCode":"japan",
+  "middleClassCode":"okinawa",
+  "smallClassCode":"nahashi",
+  "applicationid":APPLICATIONID,
+}
+# print(params)
