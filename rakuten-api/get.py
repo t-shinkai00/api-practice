1fd24bdfc57fcb23767c02212ad1814e9c7c7208
t-shinkai00 <t.shinkai00@gmail.com>
2021-10-10 22:09:47 +0900

    complete

--- rakuten-api/get.py
+++ rakuten-api/get.py
@@ -18,3 +18,23 @@ res=requests.get(url=ENDPOINT, params=params)
 
 resuluts=res.json()
 # print(resuluts)
+
+hotels=resuluts["hotels"]
+# print(hotels[0]["hotel"][0]["hotelBasicInfo"]["hotelName"])
+df=pd.DataFrame()
+for i, hotel in enumerate(hotels):
+  info=hotel["hotel"][0]["hotelBasicInfo"]
+  # print(f'Name: {info["hotelName"]}, URL: {info["hotelInformationUrl"]}, MinCharge: {info["hotelMinCharge"]}')
+  _df=pd.DataFrame(info, index=[i])
+  # print(_df)
+  df=df.append(_df)
+# print(df)
+
+# print(df.columns)
+df[[
+  'hotelName','hotelInformationUrl',
+  'hotelMinCharge', 'latitude', 'longitude', 'postalCode', 'address1',
+  'address2', 'telephoneNo', 'parkingInformation',
+  'nearestStation'
+]].to_csv("hotels.csv", index=False)
+# print(df)
