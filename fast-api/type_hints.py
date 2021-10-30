1f856dc7b5e630d192c89020bad25844c8658b71
t-shinkai00 <t.shinkai00@gmail.com>
2021-10-30 22:57:31 +0900

    add type hint of List and Dict

--- fast-api/type_hints.py
+++ fast-api/type_hints.py
@@ -6,3 +6,9 @@ def calc_price_including_tax(price: int, tax: float)-> int:
 
 if __name__ == "__main__":
   print(f"{calc_price_including_tax(price, tax)}円")
+
+
+from typing import List, Dict
+
+sample_list:List[int] = [1,2,3,4,5,6,7,8,9,10,11]
+sample_dict:Dict[str,str] = {"username":"t-shinkai00"}
