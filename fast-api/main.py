bd24539b4c57627303c50850341564bde4a9ce26
t-shinkai00 <t.shinkai00@gmail.com>
2021-11-24 16:10:38 +0900

    add nested request body app

--- fast-api/main.py
+++ fast-api/main.py
@@ -1,6 +1,20 @@
-from typing import Optional
+from typing import Optional, List
 from fastapi import FastAPI
 from pydantic import BaseModel
+
+class Item(BaseModel):
+  name: str
+  description: Optional[str] = None
+  price: int
+  tax: Optional[float] = 1.1
+
+class ShopInfo(BaseModel):
+  name: str
+  location: str
+
+class Data(BaseModel):
+  shop_info: Optional[ShopInfo] = None
+  items: List[Item]
 
 app=FastAPI()  # instance of FastAPI
 
@@ -12,13 +26,11 @@ async def index():  # asynchronous processing
 async def country(country_name: str, country_number: Optional[int]=None):  # country_number: optional query parameter
   return {"country_name": country_name, "country_number": country_number}
 
-class Item(BaseModel):
-  name: str
-  description: Optional[str] = None
-  price: int
-  tax: Optional[float] = None
-
 @app.post("/item")  # post method
 async def create_item(item: Item):
   # return {"message": f"{item.name}は、税込み価格{item.price*item.tax}円です。"}
   return item
+
+@app.post("/")
+async def index(data: Data):
+  return {"data": data}
