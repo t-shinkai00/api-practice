2af31789e26415a846600093e934f4755b7daeca
t-shinkai00 <t.shinkai00@gmail.com>
2021-11-24 14:53:57 +0900

    change page name

--- fast-api/main.py
+++ fast-api/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 app=FastAPI()  # instance of FastAPI
 
-@app.get("/")  # get from root by HTTP method
+@app.get("/hello")  # get from root by HTTP method
 async def index():  # asynchronous processing
   return {"message": "Hello World!!!"}
