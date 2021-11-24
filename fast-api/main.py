ae69752e5abf5443eaad471fcd97d2fe37d3c373
t-shinkai00 <t.shinkai00@gmail.com>
2021-11-24 15:34:50 +0900

    add request body

--- fast-api/main.py
+++ fast-api/main.py
@@ -1,5 +1,7 @@
 from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 app=FastAPI()  # instance of FastAPI
 
 @app.get("/hello")  # get from root by HTTP method
@@ -9,3 +11,14 @@ async def index():  # asynchronous processing
 @app.get("/countries/{country_name}")  # country_name: path parameter
 async def country(country_name: str, country_number: Optional[int]=None):  # country_number: optional query parameter
   return {"country_name": country_name, "country_number": country_number}
+
+class Item(BaseModel):
+  name: str
+  description: Optional[str] = None
+  price: int
+  tax: Optional[float] = None
+
+@app.post("/item/")
+async def create_item(item: Item):
+  # return {"message": f"{item.name}は、税込み価格{item.price*item.tax}円です。"}
+  return item
