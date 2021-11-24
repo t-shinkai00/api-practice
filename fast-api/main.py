fd0cf0e680326aa57f581d93d3b913b5191594fb
t-shinkai00 <t.shinkai00@gmail.com>
2021-11-24 14:57:03 +0900

    return dynamic value

--- fast-api/main.py
+++ fast-api/main.py
@@ -4,3 +4,7 @@ app=FastAPI()  # instance of FastAPI
 @app.get("/hello")  # get from root by HTTP method
 async def index():  # asynchronous processing
   return {"message": "Hello World!!!"}
+
+@app.get("/countries/{country_name}")  # get from root by HTTP method
+async def country(country_name):  # asynchronous processing
+  return {"country_name": country_name}
