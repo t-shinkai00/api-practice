7d06cde4b5d87d6439f805c96eca9a4ca1c86169
Takahiro Shinkai <t.shinkai00@gmail.com>
2021-11-25 14:55:44 +0900

    add apps

--- deploy-api/main.py
+++ deploy-api/main.py
@@ -5,3 +5,13 @@ class Data(BaseModel):
   x: float
   y: float
 
+app = FastAPI()
+
+@app.get('/')
+def index():
+  return {"message": "Hello Data!"}
+
+@app.post("/")
+def calc(data: Data):
+  z = data.x*data.y
+  return {"result": z}
